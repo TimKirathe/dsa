d99167edaba3ac907a90f043663fba9ef926ce65
Timothy Kirathe <timothy.kirathe@student.manchester.ac.uk>
2024-04-08 20:36:51 +0300

    Finished explanation of solution to delete middle of linked list problem. Also fixed the solution to work for all sizes of LinkedLists

--- delete-mid-linked-list.py
+++ delete-mid-linked-list.py
@@ -1,3 +1,14 @@
+# Task: Given nodes of a linked list consisting of a value and pointer to next node, delete the middle node of the linked list.
+
+# Conceptual Idea: Use a 'fast' pointer and a 'slow' pointer. Have the 'fast' pointer move 2 units ahead & slow pointer move one unit ahead during loop
+#                  iteration. Logic is that by the time 'fast' pointer has reached end of list, slow pointer will be at the middle of the list because
+#                  fast is moving 2x faster than slow. During execution of loop, keep variable 'prev' that stores node that comes before one pointed by
+#                  slow. When reach end of list, change pointer of prev.next to slow.next, effectively skipping over middle node at slow.
+
+# Complexity: Time complexity is O(n), because need to iterate through whole LinkedList. Space complexity is O(1), because size of fast, slow and prev
+#             variables remains constant throughout loop irrespective of size of LinkedList.
+
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -12,7 +23,10 @@ def deleteMiddle(head):
         prev = slow
         slow = slow.next
 
-    prev.next = slow.next
+    if slow == fast:
+        head = head.next
+    else:
+        prev.next = slow.next
     return head
 
 
